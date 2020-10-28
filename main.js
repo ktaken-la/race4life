6e3d5a21cb2297058c3e24e10e1288454ce22535
ktaken-la <69379811+ktaken-la@users.noreply.github.com>
2020-10-28 21:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,100 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+
+background_image="images (2).jpg"
+
+car1_width=120;
+car1_height=70;
+car1_image="car1.png"
+car1_x=10;
+car1_y=10;
+
+car2_width=120;
+car2_height=70;
+car2_image="car2.png"
+car2_x=10;
+car2_y=10;
+
+function add(){
+   background_imgTag=new Image();
+   background_imgTag.onload=uploadBackgound;
+   background_imgTag.src=background_image;
+
+   car1_imgTag=new Image();
+   car1_imgTag.onload=uploadcar1;
+   car1_imgTag.src=car1_image;
+
+   car2_imgTag=new Image();
+   car2_imgTag.onload=uploadcar2;
+   car2_imgTag.src=car2_image;
+}
+
+function uploadBackgound() {
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+}
+
+function uploadcar1() {
+    ctx.drawImage(car1_imgTag,car1_x,car1_y,car1_width,car1_height);
+}
+
+function uploadcar2() {
+    ctx.drawImage(car2_imgTag,car2_x,car2_y,car2_width,car2_height);
+}
+
+window.addEventListener("keydown",my_keydown)
+
+function my_keydown() {
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+
+    if(keyPressed=="38") 
+    {
+        up();
+        console.log("up");
+    }
+
+    if(keyPressed=="40") 
+    {
+        down();
+        console.log("down");
+    }
+
+    if(keyPressed=="37") 
+    {
+        left();
+        console.log("left");
+    }
+
+    if(keyPressed=="39") 
+    {
+        right();
+        console.log("right");
+    }
+  
+
+
+
+    if(keyPressed=="87") 
+    {
+        w();
+        console.log("key w");
+    }
+
+    if(keyPressed=="83") 
+    {
+        s();
+        console.log("key s");
+    }
+
+    if(keyPressed=="65") 
+    {
+        a();
+        console.log("key a");
+    }
+
+    if(keyPressed=="68") 
+    {
+        d();
+        console.log("key d");
+    }
+}
